2eb07f478a1fb2d01d5d89f2583ae797ddf3c03a
Ivan Melentev <ivan.saxalin@mail.ru>
2020-12-29 22:04:44 +0300

    AdditionalMethodsForExtensions

---
+++ VkNetExtensions/AdditionalMethodsForExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace VkNetExtensions
+{
+	/// <summary>
+	/// В этом файле нет методов расширения для VkNet, но сами методы расширения используют методы отсюда.
+	/// </summary>
+	public static class AdditionalMethodsForExtensions
+	{
+		/// <summary>
+		/// Получить строку в байтах
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public static byte[] GetFileTxtByte(string text)
+		{
+			return new UTF8Encoding(true).GetBytes(text);
+		}
+	}
+}
