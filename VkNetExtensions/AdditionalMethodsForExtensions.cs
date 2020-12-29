4a299bfab50ef6d1e09624f97c9b41905d779e63
Ivan Melentev <ivan.saxalin@mail.ru>
2020-12-29 22:06:28 +0300

    UploadFile

--- VkNetExtensions/AdditionalMethodsForExtensions.cs
+++ VkNetExtensions/AdditionalMethodsForExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VkNetExtensions
 {
@@ -15,6 +18,27 @@ namespace VkNetExtensions
 		public static byte[] GetFileTxtByte(string text)
 		{
 			return new UTF8Encoding(true).GetBytes(text);
+		}
+
+		/// <summary>
+		///     Загружает массив байт на указанный url
+		/// </summary>
+		/// <param name="url">Адрес для загрузки</param>
+		/// <param name="data">Массив данных для загрузки</param>
+		/// <returns>Строка, которую вернул сервер.</returns>
+		public static async Task<string> UploadFile(string url, byte[] data, string filename)
+		{
+			var index = filename.LastIndexOf('.') + 1;
+			var format = filename.Substring(index, filename.Length - index);
+			using var client = new HttpClient();
+
+			throw new NotImplementedException();
+
+			// todo расскомментить и реализовать без flurl
+			// return await url.PostMultipartAsync
+			// (
+			// 	mp => mp.AddFile("file", new MemoryStream(data), $"file.{format}")
+			// ).ReceiveString();
 		}
 	}
 }
