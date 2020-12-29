5f1a2f9c3e6c957b254e841e83e8cdcd050cdc90
Ivan Melentev <ivan.saxalin@mail.ru>
2020-12-29 22:07:50 +0300

    LoadDocumentToChatAsync

--- VkNetExtensions/VkNetExtensions.cs
+++ VkNetExtensions/VkNetExtensions.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using VkNet.Abstractions;
 using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
+using VkNet.Model.Attachments;
 
 namespace VkNetExtensions
 {
@@ -29,7 +32,7 @@ namespace VkNetExtensions
 				return info.StartDate.GetValueOrDefault();
 			}
 
-			var client = new HttpClient();
+			using var client = new HttpClient();
 			var str = await client.GetStringAsync($"https://vk.com/foaf.php?id={id}");
 			var doc = new HtmlDocument();
 			doc.LoadHtml(str);
@@ -56,6 +59,48 @@ namespace VkNetExtensions
 			if (message.ReplyMessage != null) return new[] {message.ReplyMessage};
 
 			return message.ForwardedMessages;
+		}
+
+		/// <summary>
+		///     Загружает документ на сервер ВК.
+		/// </summary>
+		/// <param name="vkApi"></param>
+		/// <param name="data">Attachment, байты которого будут отправлены на сервер</param>
+		/// <param name="docMessageType">Тип документа - документ или аудиосообщение</param>
+		/// <param name="peerId">Идентификатор назначения</param>
+		/// <param name="filename">Итоговое название документа</param>
+		/// <returns>Attachment для отправки вместе с сообщением</returns>
+		public static async Task<Attachment?> LoadDocumentToChatAsync(IVkApi vkApi, byte[] data,
+																	DocMessageType docMessageType, long peerId, string filename)
+		{
+			var uploadServer = vkApi.Docs.GetMessagesUploadServer(peerId, docMessageType);
+			var r = await AdditionalMethodsForExtensions.UploadFile(uploadServer.UploadUrl, data, filename);
+			var documents = await vkApi.Docs.SaveAsync(r, filename, null);
+
+			if (!documents.Any()) return null;
+
+			return documents.First();
+		}
+
+		/// <summary>
+		///     Загружает txt файл на сервер вк
+		/// </summary>
+		/// <param name="vkApi"></param>
+		/// <param name="text">Текст в txt документе</param>
+		/// <param name="peerId">Идентификатор назначения</param>
+		/// <param name="filename">Итоговое название документа</param>
+		/// <param name="txt">Подставлять ли тип .txt</param>
+		/// <returns></returns>
+		public static async Task<Attachment?> LoadTxtDocumentToChatAsync(IVkApi vkApi, string text, long peerId, string filename,
+																		bool txt = true)
+		{
+			var data = AdditionalMethodsForExtensions.GetFileTxtByte(text);
+
+			return await LoadDocumentToChatAsync(vkApi,
+				data,
+				DocMessageType.Doc,
+				peerId,
+				$"{filename}{(txt ? ".txt" : "")}");
 		}
 	}
 }
